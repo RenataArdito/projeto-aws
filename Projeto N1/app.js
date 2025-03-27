bcf97f1a0d1d73b39b42bd97fd1a08af988cf0ab
RenataArdito <10417520@mackenzista.com.br>
2025-03-27 22:36:33 +0000

    detalhes

--- Projeto N1/app.js
+++ Projeto N1/app.js
@@ -6,33 +6,28 @@ const path = require('path');
 const app = express();
 const port = 25000;
 
-// Configuração do CORS
+// CORS
 app.use(cors({
-    origin: '*',  // Permite que qualquer origem acesse a API. Substitua "*" pelo domínio do seu front-end, se necessário.
+    origin: '*',
 }));
 
 // Middleware para aceitar JSON e formulários
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Servir arquivos estáticos (se necessário)
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Rota para servir a página HTML principal
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-// Rota para receber as solicitações do formulário
 app.post('/api/solicitacao', (req, res) => {
     const { categoria, endereco, descricao } = req.body;
 
-    // Verificação de campos obrigatórios
     if (!categoria || !endereco || !descricao) {
         return res.status(400).json({ message: 'Todos os campos são obrigatórios' });
     }
 
-    // Log dos dados recebidos para depuração
     console.log("Dados recebidos do front-end:", req.body);
 
     // Envio de resposta de sucesso
