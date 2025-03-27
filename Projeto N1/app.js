b817d121d2ba658da7d30bc1d348f5c8910e0815
RenataArdito <10417520@mackenzista.com.br>
2025-03-27 22:32:51 +0000

    detalhes

--- Projeto N1/app.js
+++ Projeto N1/app.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const path = require('path');
 
 const app = express();
-const port = 8080;
+const port = 25000;
 
 // Configuração do CORS
 app.use(cors({
