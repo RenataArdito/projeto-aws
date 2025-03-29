ef3bc580725a96e2674ba1daa2b22bd1eb809112
RenataArdito <10417520@mackenzista.com.br>
2025-03-29 00:52:01 +0000

    detalhes

--- Projeto N1/script.js
+++ Projeto N1/script.js
@@ -13,7 +13,7 @@ document.getElementById('solicitacao-form').addEventListener('submit', async fun
     const data = { categoria, endereco, descricao };
 
     // URL do backend na instância privada da AWS
-    const apiUrl = `http://3.90.248.26:8080`;
+    const apiUrl = `http://10.0.3.11:25000/api/solicitacao`;
 
     try {
         const response = await fetch(apiUrl, { 
